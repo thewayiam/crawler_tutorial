ad5756e67eab04fa75d6c2deab11aacdcfde466f
thewayiam <twly.tw@gmail.com>
2017-03-11 14:53:36 +0800

    better python3 compatible

--- examples/1_3/parser.py
+++ examples/1_3/parser.py
@@ -8,10 +8,10 @@ p_tags = response.xpath('//p[string-length(text()) > 0]')
 for p_tag in p_tags:
     p_text = p_tag.xpath('normalize-space(string())').extract_first()
     table_tag = p_tag.xpath('following-sibling::table[1]')
-    print p_text, table_tag
+    print(p_text, table_tag)
 
 table_tags = response.xpath('//table')
 for table_tag in table_tags:
     p_tag = table_tag.xpath('preceding-sibling::p[string-length(text()) > 0][1]')
     p_text = p_tag.xpath('normalize-space(string())').extract_first()
-    print p_text, table_tag
+    print(p_text, table_tag)
--- examples/1_4/parser.py
+++ examples/1_4/parser.py
@@ -9,14 +9,14 @@ titles = response.xpath(u'//p[re:test(., "公\s*職\s*人\s*員.*表$")]')
 for title in titles:
     title_text = title.xpath('normalize-space(string())').extract_first()
     first_table = title.xpath('following-sibling::table[1]')
-    print title_text, first_table
+    print(title_text, first_table)
     p_tags = title.xpath('following::p[string-length(text()) > 0]')
     for p_tag in p_tags:
         p_text = p_tag.xpath('normalize-space(string())').extract_first()
         table_tag = p_tag.xpath('following-sibling::table[1]')
         if re.search(u'公\s*職\s*人\s*員.*表$', p_text):
             break
-        print p_text, table_tag
+        print(p_text, table_tag)
 
     table_tags = title.xpath('following-sibling::table')
     for table_tag in table_tags:
@@ -24,4 +24,4 @@ for title in titles:
         p_text = p_tag.xpath('normalize-space(string())').extract_first()
         if re.search(u'公\s*職\s*人\s*員.*表$', p_text):
             break
-        print p_text, table_tag
+        print(p_text, table_tag)
